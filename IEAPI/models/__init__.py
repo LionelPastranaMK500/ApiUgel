cf9ae6d5b0bb4defa242a6ecc0580726de264193
LionelPastranaMK500 <andriypastrana1@gmail.com>
2026-01-22 20:20:21 -0500

    refactor: Reorganizar importaciones y actualizar __all__ en el módulo de modelos

--- IEAPI/models/__init__.py
+++ IEAPI/models/__init__.py
@@ -1,13 +1,56 @@
+# IEAPI/models/__init__.py
 from .institucionEducativa import InstitucionEducativa
-from .niveleducativo import Niveleducativo
+from .nivel_catalogo import NivelCatalogo
+from .niveleducativo import InstitucionNivel
 from .periodoLectivo import PeriodoLectivo
 from .grado import Grado
 from .seccion import Seccion
+from .usuario import PerfilPersona, AsignacionRol
+from .rol import Rol
+from .asignacion_rol import PersonalPeriodo
+from .matricula import Matricula
+from .asignatura import Asignatura
+from .malla_curricular import MallaCurricular, DetalleMalla
+from .curso import Curso
+from .evaluacion import TipoEvaluacion, Evaluacion
+from .escala_calificacion import EscalaCalificacion, ValorEscala
+from .componente_evaluacion import ComponenteEvaluacion
+from .calificacion import Calificacion
+from .asistencia import AsistenciaAlumno, AsistenciaPersonal
+from .pago import ConceptoPago, ObligacionPago
+from .anuncio import Anuncio
+from .debate import DebateTema, DebatePost
+from .mensaje import Conversacion, Mensaje
 
 __all__ = [
     "InstitucionEducativa",
-    "Niveleducativo",
+    "NivelCatalogo",
+    "InstitucionNivel",
     "PeriodoLectivo",
     "Grado",
     "Seccion",
+    "PerfilPersona",
+    "AsignacionRol",
+    "Rol",
+    "PersonalPeriodo",
+    "Matricula",
+    "Asignatura",
+    "MallaCurricular",
+    "DetalleMalla",
+    "Curso",
+    "TipoEvaluacion",
+    "Evaluacion",
+    "EscalaCalificacion",
+    "ValorEscala",
+    "ComponenteEvaluacion",
+    "Calificacion",
+    "AsistenciaAlumno",
+    "AsistenciaPersonal",
+    "ConceptoPago",
+    "ObligacionPago",
+    "Anuncio",
+    "DebateTema",
+    "DebatePost",
+    "Conversacion",
+    "Mensaje",
 ]
